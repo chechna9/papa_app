422dde468af1606373719e19b38621b7b3a7017e
chechna9 <ja_bouchouareb@esi.dz>
2023-06-25 02:20:26 +0100

    added SciDeg helper

--- lib/data/datasources/sqflite_data_source.dart
+++ lib/data/datasources/sqflite_data_source.dart
@@ -1,28 +1,13 @@
+import 'package:papa_app/data/helpers/sql_helpers.dart/SciDeg_helper.dart';
 import 'package:papa_app/data/models/SciDeg_Model/sci_deg.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class SqfliteDataSource {
+class SqfliteDataSource with SciDegHelper {
   // db consts
   static const String _dbName = 'papa6.db';
   static const int _dbVersion = 1;
 
-  // helper consts
-
-  // SciDeg
-  static const String _sciDegTable = 'sci_deg';
-  static const String _sciDegId = 'id';
-  static const String _sciDegName = 'name';
-  static const String _sciDegDegree = 'degree';
-  static const List<SciDeg> _defaultSciDegs = [
-    SciDeg(id: 0, name: "دكتوراه", degree: 0),
-    SciDeg(id: 1, name: "ش د العليا رياضيات", degree: 1),
-    SciDeg(id: 2, name: "ليسانس رياضيات", degree: 2),
-    SciDeg(id: 3, name: "مهندس دولة", degree: 3),
-    SciDeg(id: 4, name: "ليسانس", degree: 4),
-    SciDeg(id: 5, name: "ماجستير رياضيات", degree: 5),
-    SciDeg(id: 6, name: "ماستر رياضيات", degree: 6),
-  ];
   // db
   Database? _db;
   Future<void> initDatabase() async {
@@ -35,17 +20,17 @@ class SqfliteDataSource {
       onCreate: (db, version) async {
         // crating SciDeg table
         await db.execute('''
-        Create Table $_sciDegTable (
-          $_sciDegId INTEGER PRIMARY KEY AUTOINCREMENT,
-          $_sciDegName TEXT NOT NULL,
-          $_sciDegDegree INTEGER NOT NULL
+        Create Table ${SciDegHelper.sciDegName} (
+          ${SciDegHelper.sciDegId} INTEGER PRIMARY KEY AUTOINCREMENT,
+          ${SciDegHelper.sciDegName} TEXT NOT NULL,
+          ${SciDegHelper.sciDegDegree} INTEGER NOT NULL
         )
         ''');
         // inserting the SciDeg into the table
         await db.transaction((txn) async {
           final batch = txn.batch();
-          for (SciDeg deg in _defaultSciDegs) {
-            batch.insert(_sciDegTable, deg.toJson());
+          for (SciDeg deg in SciDegHelper.defaultSciDegs) {
+            batch.insert(SciDegHelper.sciDegTable, deg.toJson());
           }
           await batch.commit();
         });
@@ -54,7 +39,7 @@ class SqfliteDataSource {
   }
 
   Future<List<SciDeg>> getSciAllDegs() async {
-    final rawResult = await _db!.query(_sciDegTable);
+    final rawResult = await _db!.query(SciDegHelper.sciDegTable);
     List<SciDeg> result = [];
     for (Map<String, dynamic> raw in rawResult) {
       result.add(SciDeg.fromJson(raw));
---
+++ lib/data/helpers/sql_helpers.dart/SciDeg_helper.dart
@@ -0,0 +1,18 @@
+import 'package:papa_app/data/models/SciDeg_Model/sci_deg.dart';
+
+mixin SciDegHelper {
+  // SciDeg
+  static const String sciDegTable = 'sci_deg';
+  static const String sciDegId = 'id';
+  static const String sciDegName = 'name';
+  static const String sciDegDegree = 'degree';
+  static const List<SciDeg> defaultSciDegs = [
+    SciDeg(id: 0, name: "دكتوراه", degree: 0),
+    SciDeg(id: 1, name: "ش د العليا رياضيات", degree: 1),
+    SciDeg(id: 2, name: "ليسانس رياضيات", degree: 2),
+    SciDeg(id: 3, name: "مهندس دولة", degree: 3),
+    SciDeg(id: 4, name: "ليسانس", degree: 4),
+    SciDeg(id: 5, name: "ماجستير رياضيات", degree: 5),
+    SciDeg(id: 6, name: "ماستر رياضيات", degree: 6),
+  ];
+}
