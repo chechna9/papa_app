e2784bcc4d5d916d41c51f382794cf544ae4b737
chechna9 <ja_bouchouareb@esi.dz>
2023-06-25 04:03:41 +0100

    school sql hepler done

---
+++ lib/data/helpers/sql_helpers.dart/school_sql_helper.dart
@@ -0,0 +1,24 @@
+mixin SchoolSqlHelper {
+  static const String schoolTable = 'school';
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String city = 'city';
+  static const String phoneNumber = 'phoneNumber';
+  static const String email = 'email';
+  static const String directorId = 'directorId';
+  static const String cordTeacherId = 'cordTeacherId';
+
+  static String createTableSchool() => '''
+  CREATE TABLE $schoolTable(
+    $id INTEGER PRIMARY KEY AUTOINCREMENT,
+    $name TEXT NOT NULL,
+    $city TEXT NOT NULL,
+    $phoneNumber TEXT NOT NULL,
+    $email TEXT NOT NULL,
+    $directorId INTEGER NOT NULL,
+    $cordTeacherId INTEGER NOT NULL,
+    FOREIGN KEY ($directorId) REFERENCES director ($id),
+    FOREIGN KEY ($cordTeacherId) REFERENCES teacher ($id)
+  );
+  ''';
+}
